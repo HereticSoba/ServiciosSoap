f1ffdf63b52a699cece47f096bfa954b90e3fd4b
HereticSoba <diegofaridbv@gmail.com>
2026-09-18 09:57:14 -0500

    Añadido faltante

--- src/main/java/com/solorzano/WebServiceConfig.java
+++ src/main/java/com/solorzano/WebServiceConfig.java
@@ -1,24 +1,29 @@
 package com.solorzano;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
 @Configuration
-public class WebServiceConfig extends WsConfigurerAdapter{
+public class WebServiceConfig extends WsConfigurerAdapter {
 	@Bean
-	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext){
-		 MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-		 servlet.setApplicationContext(applicationContext);
-		 servlet.setTransformSchemaLocations(true);
-		 return new ServletRegistrationBean<>(servlet, "/ws/");
+	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(
+			ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformSchemaLocations(true);
+		return new ServletRegistrationBean<>(servlet, "/ws/");
 	}
-	
+
 	@Bean(name = "paises")
 	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema paisesSchema) {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
@@ -28,5 +33,10 @@ public class WebServiceConfig extends WsConfigurerAdapter{
 		wsdl11Definition.setSchema(paisesSchema);
 		return wsdl11Definition;
 	}
-	
+
+	@Bean
+	public XsdSchema paisesSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("paises.xsd"));
+	}
+
 }
